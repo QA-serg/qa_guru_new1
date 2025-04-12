29e56b953c8aa76b835c21ba42a41833ad30ba80
QA-serg <Galimov.serejka@mail.ru>
2025-04-12 13:09:28 +0300

    add getMassage() method

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -6,4 +6,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello world!");
         }
+
+        new*
+    private static String getMessage () {
+        return "Hello dodo";
+    }
     }
